5fc726833e02ec5b61e890e82805201db7a7dfd5
HuangXinHao <gary8222558@gmail.com>
2020-10-23 16:51:00 +0800

    new

---
+++ uberpanda/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <router-link to="/">Go to </router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+    <router-link to="/foo">Go to Foo</router-link>
+     <router-view></router-view>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
